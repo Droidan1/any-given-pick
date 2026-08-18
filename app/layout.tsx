f23fc7a92ce95c463a402ad0c4928fc600adbd2c
Brian Howard <howardbrian86@gmail.com>
2026-08-18 08:51:00 -0400

    Fix iPhone PWA status bar blur

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
   },
   appleWebApp: {
     capable: true,
-    statusBarStyle: "black-translucent",
+    statusBarStyle: "black",
     title: "Any Given Pick",
   },
   openGraph: {
